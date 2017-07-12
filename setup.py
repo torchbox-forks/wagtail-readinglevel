b35d1fcd4ac7ea0b9cf288070a6165319e8012dd
Chris Shaw <chris@vix.digital>
2017-07-12 20:07:13 +0100

    Added find packaged import and a wagtail requirements

--- setup.py
+++ setup.py
@@ -1,11 +1,19 @@
-from setuptools import setup
+#!/usr/bin/env python
+"""
+Wagtail readinglevel plugin that adds reading level check functionality to rich text editors.
+"""
 
-setup(name='wagtailreadinglevel',
+from setuptools import setup, find_packages
+
+setup(name='wagtail-readinglevel',
       version='0.1',
       description='Determine reading age of a piece of text.',
       url='http://github.com/vixdigital/wagtail-readinglevel',
       author='VIX Digital',
       author_email='info@vix.digital',
       license='MIT',
-      packages=['wagtailreadinglevel'],
+      packages=find_packages(),
+      install_requires=[
+          'wagtail>=1.0',
+      ],
       zip_safe=False)
