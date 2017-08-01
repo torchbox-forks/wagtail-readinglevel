fe79698ad406e19de94ba0164bba33e731337828
Chris Shaw <chris@vix.digital>
2017-08-01 15:42:52 +0100

    Implemented parseing of streamfield rtfs Implemented parseing of streamfield rich text fields and the calculations needed to work out character count, word count and sentence count which are all commonly required for figuring out reading level/age. These are currently displayed in the help text area. This does not currently cover regualar (non streamfield rich text fields, I will port this functionality over once its complete).

--- readinglevel/static/hallo-readinglevel-plugin.js
+++ readinglevel/static/hallo-readinglevel-plugin.js
@@ -18,6 +18,11 @@ $(function (){
                 // Didn't find an existing help box so create one with our custom class
                 richTextParent.append("<div style='opacity:1;' class='object-help help readinglevel'>Test help text</div");
             }
+            $(richTextArea.find('.richtext')).bind('hallomodified', function(event) {
+                var text = event.currentTarget.innerText;
+                console.log("INNER TEXT");
+                console.log(text);
+            });
         });
     }
 
@@ -26,6 +31,7 @@ $(function (){
 	var widget_rich_text_areas = $('.widget-hallo_rich_text_area .richtext');
 
     if(widget_rich_text_areas) {
+
         // Loop through the streamfield rich text areas
         widget_rich_text_areas.each(function() {
             // Get the current rich text area for this loop the parent area
@@ -40,6 +46,27 @@ $(function (){
                 // Didn't find an existing help box so create one with our custom class
                 richTextParent.append("<p class='help readinglevel'>Test help text</div");
             }
+            richTextArea.on('hallomodified', function(event, data) {
+                var text = $(richTextArea).text()
+                
+                // Calculate the character count
+                var charCount = text.length;    
+
+                // Calculate the word count
+                var wordCount = text.split(" ").length;
+
+                // Calculate the sentence count
+                var sentenceCount = (text.replace(/\w[.?!](\s|$)/g, "$1|").split("|").length) - 1;
+
+                // If we have an empty first value in the array we know our text box is actually empty
+                // so we need to minus 1 from our word count
+                if(text.split(" ")[0] == "") {
+                    wordCount -= 1;
+                }
+
+                // Modify the help area to include the new information
+                richTextParent.find('p.help').text("Char count: " + charCount +  " Word count: " + wordCount + " Sentences: " + sentenceCount);
+            });
         });
     }
 });
