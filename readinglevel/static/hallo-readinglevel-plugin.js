0a79e07e0aea6fd0892d854862887d72903c9f17
Chris Shaw <chris@vix.digital>
2017-08-02 10:45:24 +0100

    Implemented the calculation func on normal rtfs Implemented the calculation functionality on normal (non streamfield) rich text fields

--- readinglevel/static/hallo-readinglevel-plugin.js
+++ readinglevel/static/hallo-readinglevel-plugin.js
@@ -5,6 +5,13 @@ $(function (){
 
     // Loop through the regular rich text areas
     if(regular_rich_text_areas) {
+        // Constants for our reading level calculation
+        // These are part of the Automated Readability Index calculation
+        // https://en.wikipedia.org/wiki/Automated_readability_index
+        var CHARACTER_WEIGHT = 4.71;
+        var SENTENCE_WEIGHT = 0.5;
+        var BASE = 21.43;
+
         regular_rich_text_areas.each(function() {
             // Get the current rich text area for this loop the parent area
             // and the help text container so we can target and modify them
@@ -18,10 +25,41 @@ $(function (){
                 // Didn't find an existing help box so create one with our custom class
                 richTextParent.append("<div style='opacity:1;' class='object-help help readinglevel'>Test help text</div");
             }
-            $(richTextArea.find('.richtext')).bind('hallomodified', function(event) {
-                var text = event.currentTarget.innerText;
-                console.log("INNER TEXT");
-                console.log(text);
+            richTextArea.on('hallomodified', function(event, data) {
+                var text = $(richTextArea).text()
+
+                var charCount = 0;
+                var wordCount = 0;
+                var sentenceCount = 0;
+                
+                // Calculate the character count
+                charCount = text.length;    
+
+                // Calculate the word count
+                wordCount = text.split(" ").length;
+
+                // Calculate the sentence count
+                sentenceCount = (text.replace(/\w[.?!](\s|$)/g, "$1|").split("|").length) - 1;
+
+                // If we have an empty first value in the array we know our text box is actually empty
+                // so we need to minus 1 from our word count
+                if(text.split(" ")[0] == "") {
+                    wordCount -= 1;
+                }
+
+                var readabilityScore = (CHARACTER_WEIGHT * (charCount / wordCount)) 
+                    + (SENTENCE_WEIGHT * (wordCount / sentenceCount)) - BASE;
+
+                var readingAge = (readabilityScore + 4.).toFixed(1);
+
+                // Modify the help area to include the new information
+                if (isFinite(readingAge)) {
+                    if (readingAge > 18) { readingAge = "18+" }
+                    richTextParent.find('div.help').html("<strong>Reading age:</strong> " + readingAge);
+                }
+                else {
+                    richTextParent.find('div.help').html("<strong>Reading age:</strong> Not enough content to calculate age score.");
+                }
             });
         });
     }
