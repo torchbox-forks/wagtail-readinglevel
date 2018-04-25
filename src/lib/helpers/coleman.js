fb17d24deb71b78601ac8cebeafc905ad602098c
Chris Shaw <chris@vix.digital>
2018-04-25 10:35:20 +0100

    Set up the coleman plugin files - Set up the coleman plugin files just returning 0 for now. - Added the coleman webpack build to the build script

---
+++ src/lib/helpers/coleman.js
@@ -0,0 +1,14 @@
+'use strict';
+
+function CalculateColemanIndex(text) {
+    var data = {
+        index: 0
+    }
+
+    return data;
+}
+
+/**
+ * Export
+ */
+module.exports = CalculateColemanIndex;
