b86e841c16dc452f6d2c1b4f5b761ed46b99b4bc
Nick Moreton <nick.moreton@torchbox.com>
2023-10-19 15:28:39 +0100

    Drop back to wagtail 2.0+

--- setup.py
+++ setup.py
@@ -17,6 +17,6 @@ setup(name='wagtail-readinglevel',
       packages=find_packages(),
       include_package_data=True,
       install_requires=[
-          'wagtail>=4.1',
+          'wagtail>=2.0',
       ],
       zip_safe=False)
