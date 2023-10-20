4af61830f8cd328813ceea21e6972904912e88e3
Nick Moreton <nick.moreton@torchbox.com>
2023-10-20 16:30:30 +0100

    Update wagtail hooks

--- wagtailreadinglevel/wagtail_hooks.py
+++ wagtailreadinglevel/wagtail_hooks.py
@@ -2,31 +2,21 @@
 Registers the functions for our plugin.
 On loading, Wagtail will find this file and execute the contents.
 """
-from __future__ import unicode_literals
-
-# Wagtail core imports
-from wagtail.admin.rich_text.editors.draftail import features as draftail_features
 from wagtail import hooks
+from wagtail.admin.rich_text.editors.draftail.features import ControlFeature
 
 
 @hooks.register('register_rich_text_features')
 def register_readinglevel_feature(features):
-    """
-    Registering the `readinglevel` feature, which shows a reading age in the Draftail editor,
-    calculated using the Automated Readability Index
-    """
     feature_name = 'readinglevel'
-    type_ = feature_name.upper()
-    control = {
-        'type': type_,
-        'description': 'Reading level',
-    }
+    features.default_features.append(feature_name)
+
     features.register_editor_plugin(
         'draftail',
         feature_name,
-        draftail_features.EntityFeature(
-            control,
-            js=['api-monkeypatch.js', 'wagtailreadinglevel.bundle.js']
-        )
+        ControlFeature({
+            'type': feature_name,
+        },
+        js=['wagtailreadinglevel.bundle.js'],
+        ),
     )
-    features.default_features.append(feature_name)
